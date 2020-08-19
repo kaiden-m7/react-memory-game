a83e530301000bf7a6b943119b9b245761b2c355
Kaiden Munsey <kaidenmunsey7@gmail.com>
2020-08-18 22:29:53 -0400

    Updates

---
+++ precache-manifest.e8bbeb91e3297a65c2b72e3a8e274d2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4258a69a32190345c3091cbf1fa906d6",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "7357f0979fb33bc76363",
+    "url": "/react-memory-game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7647f54b2dc53e891760",
+    "url": "/react-memory-game/static/js/2.55563123.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-memory-game/static/js/2.55563123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7357f0979fb33bc76363",
+    "url": "/react-memory-game/static/js/main.29c6e600.chunk.js"
+  },
+  {
+    "revision": "1a86dceb82ffaca3f9cf",
+    "url": "/react-memory-game/static/js/runtime-main.062bf78b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-memory-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
